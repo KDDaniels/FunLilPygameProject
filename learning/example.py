3ad73c03edf9c53f6b7c901a1e26bb4dbf3c427f
AgehWewt <kendall.daniels.dev@gmail.com>
2025-02-12 12:27:05 -0600

    added basic example

---
+++ learning/example.py
@@ -0,0 +1,37 @@
+# shoutout DaFluffyPotato
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 640))
+
+potato_img = pygame.image.load("potato.png").convert_alpha()
+
+potato_img = pygame.transform.scale(potato_img,
+                                    (potato_img.get_width() / 2,
+                                     potato_img.get_height() / 2))
+
+running = True
+x = 0
+clock = pygame.time.Clock()
+
+
+while running:
+    screen.fill((255, 255, 255))
+
+    screen.blit(potato_img, (x, 30))
+
+    x += 1
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+    delta_time = clock.tick(60)
+    delta_time = max(0.001, min(0.1, delta_time))
+
+
+pygame.quit()
