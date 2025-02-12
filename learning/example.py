a8583c3abc17cd094ffdac49a780bf745404c7f5
AgehWewt <kendall.daniels.dev@gmail.com>
2025-02-12 12:37:30 -0600

    added delta_time

--- learning/example.py
+++ learning/example.py
@@ -16,13 +16,15 @@ running = True
 x = 0
 clock = pygame.time.Clock()
 
+delta_time = 0.1
+
 
 while running:
     screen.fill((255, 255, 255))
 
     screen.blit(potato_img, (x, 30))
 
-    x += 1
+    x += 50 * delta_time
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -30,7 +32,7 @@ while running:
 
     pygame.display.flip()
 
-    delta_time = clock.tick(60)
+    delta_time = clock.tick(60) / 1000
     delta_time = max(0.001, min(0.1, delta_time))
 
 
